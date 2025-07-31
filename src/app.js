25f22284eefa91bf8ccf2c4385490d6cdf864ad3
additisharma1203 <additisharma.0312@gmail.com>
2025-07-31 12:15:31 +0530

    playing with multiple routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 const express=require('express')
 const app=express();
 
+// order is very imp  => as i only ('/) is used as the first route then 
+// all other will give same output as of it , (SO USE '/' AT THE LAST) 
+
+//  if /hello ans /hello/2   (then first use /hello/2  (to use it effectively) )
+
+app.use('/hello',(req,res)=>{
+    res.send("Hello hello")  // reqhandler
+})
+app.get('/user',(req,res)=>{
+    res.send({firstname:"aditi",lastnmae:"sharma"})
+})
+
+app.post('/user',(req,res)=>{
+    res.send("data saved successfully")
+})
 app.use('/',(req,res)=>{
     res.send("Hello from server")  // reqhandler
 })
@@ -8,4 +23,6 @@ app.use('/',(req,res)=>{
 app.listen(3000,()=>{
     console.log("server is successfully listenning on 3000")
 })
+
+/////////////  HTTP METHODS (get,post,put, delete)
 
